09a711128e3781cc8677c321a1a8562eee6bdaa3
Carly Sandler <carly.sandler@gmail.com>
2022-01-11 13:07:04 -0500

    User model updated

---
+++ server/db/models/Product.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize')
+const { STRING, INTEGER, TEXT, DECIMAL } = Sequelize
+const db = require('../db')
+const axios = require('axios');
+
+
+
+
+
+
--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize')
+const { STRING, VIRTUAL, BOOLEAN } = Sequelize
 const db = require('../db')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
@@ -8,12 +9,48 @@ const SALT_ROUNDS = 5;
 
 const User = db.define('user', {
   username: {
-    type: Sequelize.STRING,
+    type: STRING,
     unique: true,
     allowNull: false
   },
   password: {
-    type: Sequelize.STRING,
+    type: STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+      notEmpty: true
+    }
+  },
+  firstName: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  fullName: {
+    type: VIRTUAL,
+    get() {
+      return this.firstName + " " + this.lastName
+    }
+  },
+  isAdmin: {
+    type: BOOLEAN,
+    defaultValue: false
   }
 })
 
@@ -34,8 +71,8 @@ User.prototype.generateToken = function() {
 /**
  * classMethods
  */
-User.authenticate = async function({ username, password }){
-    const user = await this.findOne({where: { username }})
+User.authenticate = async function({ email, password }){
+    const user = await this.findOne({where: { email }})
     if (!user || !(await user.correctPassword(password))) {
       const error = Error('Incorrect username/password');
       error.status = 401;
@@ -49,11 +86,38 @@ User.findByToken = async function(token) {
     const {id} = await jwt.verify(token, process.env.JWT)
     const user = User.findByPk(id)
     if (!user) {
-      throw 'nooo'
+      throw 'Unable to find user'
     }
     return user
   } catch (ex) {
     const error = Error('bad token')
+    error.status = 401
+    throw error
+  }
+}
+
+// Lookup user by email: If no user, create a user -> Look up order history!
+User.lookupByEmail = async function ({ firstName, lastName, username, email, password }) {
+  try {
+    let user = await User.findOne({
+      where: {
+      email: email,
+      username: username
+      }
+    });
+    if (!user) {
+      user = await User.create({
+        firstName,
+        lastName,
+        email,
+        username,
+        password
+
+      })
+    }
+    return user
+  } catch (ex) {
+    const error = Error('Username or Email is not valid')
     error.status = 401
     throw error
   }
@@ -69,6 +133,17 @@ const hashPassword = async(user) => {
   }
 }
 
+const emailToLowerCase = async(user) => {
+  if (user.changed('email')) {
+    user.email = user.email.toLowerCase()
+  }
+}
+
+
+
 User.beforeCreate(hashPassword)
 User.beforeUpdate(hashPassword)
 User.beforeBulkCreate(users => Promise.all(users.map(hashPassword)))
+
+User.beforeUpdate(emailToLowerCase);
+User.beforeBulkCreate(users => Promise.all(users.map(emailToLowerCase)))
