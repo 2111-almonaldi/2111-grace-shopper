49c790063ae20ce7c4ee2b3a66fb84ed7a1a1f0f
Nicole DeSantis <nicolemdesantis13@gmail.com>
2022-01-14 18:42:09 +0000

    fix: Orders not seeding successfully in DB

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,12 @@
 //this is the access point for all things database related!
 
-const db = require('./db');
+const db = require("./db");
 
-const { User } = require('./models/User');
-const { Product } = require('./models/Product');
-const { ProductCategory } = require('./models/ProductCategory');
+const { User } = require("./models/User");
+const { Product } = require("./models/Product");
+const { ProductCategory } = require("./models/ProductCategory");
 // const { OrderDetails } = require('./models/OrderDetails');
-const { Order } = require('./models/Order');
+const { Order } = require("./models/Order");
 
 //associations could go here!
 
@@ -16,31 +16,31 @@ Order.belongsTo(User);
 
 // ORDER VS PRODUCT
 Order.hasMany(Product);
-Product.belongsTo(Order);
+Product.belongsTo(Order, { as: "items" });
 
 // Order.belongsToMany(Product, { through: OrderDetails });
 // Product.belongsToMany(Order, { through: OrderDetails });
 
 // USER VS PRODUCT - make a wishllist ? later on...
-User.belongsToMany(Product, { through: 'join-product-user', as: 'wishlist' });
-Product.belongsToMany(User, { through: 'join-product-user', as: 'wishlist' });
+User.belongsToMany(Product, { through: "join-product-user", as: "wishlist" });
+Product.belongsToMany(User, { through: "join-product-user", as: "wishlist" });
 
 // PRODUCT VS PRODUCTCATEGORY
 Product.belongsToMany(ProductCategory, {
-	through: 'join-product-category',
-	as: 'categories',
+  through: "join-product-category",
+  as: "categories",
 });
 ProductCategory.belongsToMany(Product, {
-	through: 'join-product-category',
-	as: 'categories',
+  through: "join-product-category",
+  as: "categories",
 });
 
 module.exports = {
-	db,
-	models: {
-		User,
-		Order,
-		Product,
-		ProductCategory,
-	},
+  db,
+  models: {
+    User,
+    Order,
+    Product,
+    ProductCategory,
+  },
 };
