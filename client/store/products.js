f25fccad6b5851907d58395e83bc9cdd0429df84
kayda <kejda.b.celaj@gmail.com>
2022-01-14 22:31:19 -0500

    fit:added the reducer for AddProduct

--- client/store/products.js
+++ client/store/products.js
@@ -2,12 +2,17 @@ import axios from "axios";
 
 //Action Types
 const GOT_PRODUCTS = "GOT_PRODUCTS";
+const ADD_PRODUCT = "ADD_PRODUCT";
 
 //Action Creators
 const gotProducts = (products) => ({
   type: GOT_PRODUCTS,
   products,
 });
+const addProduct = (product) => ({
+  type: ADD_PRODUCT,
+  product
+})
 
 //Thunk Creator
 export const loadProducts = () => {
@@ -21,6 +26,14 @@ export const loadProducts = () => {
   };
 };
 
+export const addNewProduct = (data, history) => {
+  return async (dispatch) => {
+    const { data: created } = await axios.post(`/api/products`, data);
+    dispatch(addProduct(created));
+    history.push(`/products`);
+  }
+}
+
 //Reducer
 const initialState = [];
 
@@ -28,6 +41,8 @@ export default function productsReducer(state = initialState, action) {
   switch (action.type) {
     case GOT_PRODUCTS:
       return action.products;
+    case ADD_PRODUCT:
+      return [...state, action.product]
     default:
       return state;
   }
