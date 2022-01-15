de5e5337946a7bc04559da030ac04762555dd9bc
kayda <kejda.b.celaj@gmail.com>
2022-01-15 14:08:18 -0500

    feat: reducer for UpdateProduct

--- client/store/products.js
+++ client/store/products.js
@@ -3,14 +3,21 @@ import axios from "axios";
 //Action Types
 const GOT_PRODUCTS = "GOT_PRODUCTS";
 const ADD_PRODUCT = "ADD_PRODUCT";
+const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 
 //Action Creators
 const gotProducts = (products) => ({
   type: GOT_PRODUCTS,
   products,
 });
+
 const addProduct = (product) => ({
   type: ADD_PRODUCT,
+  product
+})
+
+const changeProduct = (product) => ({
+  type: UPDATE_PRODUCT,
   product
 })
 
@@ -34,6 +41,14 @@ export const addNewProduct = (data, history) => {
   }
 }
 
+export const updateProduct = (product, history) => {
+  return async (dispatch) => {
+    const { data: updated } = await axios.put(`api/products/${product.id}`, product);
+    dispatch(updateProduct(updated));
+    history.push(`/products`);
+  }
+}
+
 //Reducer
 const initialState = [];
 
@@ -41,6 +56,8 @@ export default function productsReducer(state = initialState, action) {
   switch (action.type) {
     case GOT_PRODUCTS:
       return action.products;
+    case UPDATE_PRODUCT:
+      return state.map((product) => product.id === action.product.id ? action.product : product);
     case ADD_PRODUCT:
       return [...state, action.product]
     default:
