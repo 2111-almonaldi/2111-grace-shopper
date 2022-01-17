14fc8fbe4c4e6a0869e85390f83d0b6f8a1e74a4
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-01-17 10:06:09 -0500

    *ignore this.

--- client/Routes.js
+++ client/Routes.js
@@ -1,67 +1,72 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import AllProducts from "./components/AllProducts";
-import SingleProduct from "./components/SingleProduct";
-import AddProduct from "./components/AddProduct";
-import UpdateProduct from "./components/UpdateProduct";
-import { Login, Signup } from "./components/AuthForm";
-import Home from "./components/Home";
-import Cart from "./components/Cart";
-import { me } from "./store";
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import AllProducts from './components/AllProducts';
+import SingleProduct from './components/SingleProduct';
+import AddProduct from './components/AddProduct';
+import UpdateProduct from './components/UpdateProduct';
+import { Login, Signup } from './components/AuthForm';
+// import AccountEdit from './components/AccountEdit';
+import Orders from './components/User/Orders';
+// import OrderDetails from './components/OrderDetails';
+// import UserMain from './components/UserMain';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import { me } from './store';
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-  componentDidMount() {
-    // this.props.loadInitialData();
-  }
+	componentDidMount() {
+		// this.props.loadInitialData();
+	}
 
-  render() {
-    const { isLoggedIn } = this.props;
+	render() {
+		const { isLoggedIn } = this.props;
 
-    return (
-        <div>
-          {isLoggedIn ? (
-            <Switch>
-              <Route path="/home" component={Home} />
-              <Route path="/cart" component={Cart} />
-            </Switch>
-          ) : (
-            <Switch>
-              <Route path="/" exact component={Login} />
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={Signup} />
-              <Route exact path="/products" component={AllProducts} />
-              <Route path="/products/create" component={AddProduct} />
-              <Route path="/products/:id/update" component={UpdateProduct} />
-              <Route path="/products/:id" component={SingleProduct} />
-              <Route path="/cart" component={Cart} />
-            </Switch>
-          )}
-        </div>
-    );
-  }
+		return (
+			<div>
+				{isLoggedIn ? (
+					<Switch>
+						<Route path="/home" component={Home} />
+						<Route path="/cart" component={Cart} />
+					</Switch>
+				) : (
+					<Switch>
+						<Route path="/" exact component={Login} />
+						<Route path="/login" component={Login} />
+						<Route path="/signup" component={Signup} />
+						<Route exact path="/products" component={AllProducts} />
+						<Route path="/products/create" component={AddProduct} />
+						<Route path="/products/:id/update" component={UpdateProduct} />
+						<Route path="/products/:id" component={SingleProduct} />
+						<Route path="/orders" component={Orders} />
+						<Route path="/cart" component={Cart} />
+					</Switch>
+				)}
+			</div>
+		);
+	}
 }
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id,
-  };
+	return {
+		// Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+		// Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+		isLoggedIn: !!state.auth.id,
+	};
 };
 
 const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(me());
-    },
-  };
+	return {
+		loadInitialData() {
+			dispatch(me());
+		},
+	};
 };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
---
+++ client/components/User/Orders.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import '../../../public/userOrders.css';
+
+function Orders() {
+	return (
+		<div className="orders">
+			<h1>Your Orders</h1>
+		</div>
+	);
+}
+
+export default Orders;
