6f39c6cb75b26d92afa7ce77a4798213ef44963b
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-01-18 13:38:48 -0500

    Navbar.js - more ui updated

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -18,6 +18,7 @@ const Navbar = ({ handleClick, isLoggedIn, cart }) => (
 						alt=""
 					/>
 				</Link>
+				<div className="header-space"></div>
 				<div className="header-nav">
 					<div className="header-option">
 						{/* The navbar will show these links after you log in */}
@@ -32,72 +33,64 @@ const Navbar = ({ handleClick, isLoggedIn, cart }) => (
 						<a href="#" onClick={handleClick}>
 							<span className="header-optionLineFour">Logout</span>
 						</a>
-					</div>
 
-					<div className="header-optionBasket">
-						{cart.length === 0 ? (
-							<Link
-								to="/cart"
-								className="header-optionLineTwo header-basketCount"
-							>
-								<ShoppingCartRoundedIcon />
-							</Link>
-						) : (
-							<Link
-								to="/cart"
-								className="header-optionLineTwo header-basketCount"
-							>
-								<ShoppingCartRoundedIcon />(
-								{cart.reduce((a, c) => a + c.count, 0)})
-							</Link>
-						)}
+						<div className="header-optionBasket">
+							{cart.length === 0 ? (
+								<Link to="/cart">
+									<ShoppingCartRoundedIcon />
+								</Link>
+							) : (
+								<Link to="/cart">
+									<ShoppingCartRoundedIcon />(
+									{cart.reduce((a, c) => a + c.count, 0)})
+								</Link>
+							)}
+						</div>
 					</div>
 				</div>
 			</div>
 		) : (
-			<div>
-				<div className="header">
-					<Link to="/home">
-						<img
-							className="header-image"
-							src="https://i.pinimg.com/564x/10/f8/bd/10f8bdfb863dee8714e3594e4c79b563.jpg"
-							alt=""
-						/>
-					</Link>
-					<div className="header-nav"></div>
-					<div className="header-option">
-						{/* The navbar will show these links before you log in */}
-						<span className="header-optionLineOne">
-							<Link to="/login">Login</Link>
-						</span>
+			<div className="header">
+				<Link to="/home">
+					<img
+						className="header-image"
+						src="https://i.pinimg.com/564x/10/f8/bd/10f8bdfb863dee8714e3594e4c79b563.jpg"
+						alt=""
+					/>
+				</Link>
+				<div className="header-nav"></div>
+				<div className="header-option">
+					{/* The navbar will show these links before you log in */}
+					<span className="header-optionLineOne">
+						<Link to="/login">Login</Link>
+					</span>
 
-						<span className="header-optionLineTwo">
-							<Link to="/signup">Sign Up</Link>
-						</span>
+					<span className="header-optionLineTwo">
+						<Link to="/signup">Sign Up</Link>
+					</span>
 
-						<span className="header-optionLineThree">
-							<Link to="/products">Products</Link>
-						</span>
-					</div>
+					<span className="header-optionLineThree">
+						<Link to="/products">Products</Link>
+					</span>
+				</div>
 
-					<div className="header-optionBasket">
-						{cart.length === 0 ? (
-							<Link
-								to="/cart"
-								className="header-optionLineTwo header-basketCount"
-							>
-								<ShoppingCartRoundedIcon />
-							</Link>
-						) : (
-							<Link
-								to="/cart"
-								className="header-optionLineTwo header-basketCount"
-							>
-								<ShoppingCartRoundedIcon />(
-								{cart.reduce((a, c) => a + c.count, 0)})
-							</Link>
-						)}
-					</div>
+				<div className="header-optionBasket">
+					{cart.length === 0 ? (
+						<Link
+							to="/cart"
+							className="header-optionLineTwo header-basketCount"
+						>
+							<ShoppingCartRoundedIcon />
+						</Link>
+					) : (
+						<Link
+							to="/cart"
+							className="header-optionLineTwo header-basketCount"
+						>
+							<ShoppingCartRoundedIcon />(
+							{cart.reduce((a, c) => a + c.count, 0)})
+						</Link>
+					)}
 				</div>
 			</div>
 		)}
