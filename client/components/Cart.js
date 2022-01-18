678365a7351f66a8454d07df75911c477edd143c
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-01-17 19:00:22 -0500

    Cart.js, cart.css - updated UI

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -7,7 +7,11 @@ import {
 	decreaseItem,
 	clearCart,
 } from '../store/cart';
+import '../../public/cart.css';
+import '../../public/subtotal.css';
 import DeleteIcon from '@mui/icons-material/Delete';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
 //import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
 
 export class Cart extends Component {
@@ -25,56 +29,67 @@ export class Cart extends Component {
 				<h3>Cart Details</h3>
 				<div>
 					{cart.length === 0 ? (
-						<div>
-							Cart is Empty—<Link to="/products">Start Browsing!</Link>
+						<div className="cart-container">
+							Cart is Empty<Link to="/products">Start Browsing!</Link>
 						</div>
 					) : (
-						<div>
-							<div>{cart.reduce((a, c) => a + c.count, 0)} items in Cart:</div>
-							<div>
+						<div className="cart">
+							<div className="cart-container">
 								{cart.map((item, idx) => (
 									<div key={idx}>
+										<img className="cart-itemImage" src={item.imageUrl}></img>
+										<strong className="cart-itemName">{item.name}</strong>
 										<div>
-											<img src={item.imageUrl}></img>
+											<IndeterminateCheckBoxIcon
+												className="cart-itemQty"
+												onClick={() => this.props.remove(item)}
+											/>
+											{item.count}
+											<AddBoxIcon
+												className="cart-itemQty"
+												onClick={() => this.props.add(item)}
+											/>
 										</div>
-										<div>
-											<div>{item.name}</div>
-											<div>
-												<button onClick={() => this.props.remove(item)}>
-													-
-												</button>{' '}
-												{item.count}{' '}
-												<button onClick={() => this.props.add(item)}>+</button>{' '}
-												<strong>{item.price}</strong>
-												<button onClick={() => this.props.removeFromCart(item)}>
-													<DeleteIcon />
-												</button>
-											</div>
-										</div>
+										<div className="cart-itemPrice">${item.price}</div>
+
+										<DeleteIcon
+											className="cart-itemDelete"
+											onClick={() => this.props.removeFromCart(item)}
+										/>
 									</div>
 								))}
 							</div>
-							{cart.length !== 0 && (
-								<div>
-									<hr />
+							<div>
+								{cart.length !== 0 && (
 									<div>
-										<div>
-											<strong>Subtotal:</strong> ${itemsPrice.toFixed(2)}
-										</div>
-										<div>
-											<strong>Tax:</strong> ${tax.toFixed(2)}
-										</div>
 										<hr />
-										<div>
-											<strong>Order Total:</strong> ${totalPrice.toFixed(2)}
+										<div className="subtotal">
+											<div className="subtotal-container">
+												<strong>Subtotal:</strong> ${itemsPrice.toFixed(2)}
+											</div>
+											<div>
+												{cart.reduce((a, c) => a + c.count, 0)} items in Cart:
+											</div>
+											<div>
+												<strong>Tax:</strong> ${tax.toFixed(2)}
+											</div>
+											<hr />
+											<div>
+												<strong>Order Total:</strong> ${totalPrice.toFixed(2)}
+											</div>
+											<button className="subtotal-button">
+												<Link to={`/checkout`}>Proceed to Checkout</Link>
+											</button>
 										</div>
-										<button>
-											<Link to={`/checkout`}>Proceed to Checkout</Link>
-										</button>
 									</div>
-								</div>
-							)}
-							<button onClick={() => this.props.clearCart()}>Clear Cart</button>
+								)}
+								<button
+									className="subtotal-button"
+									onClick={() => this.props.clearCart()}
+								>
+									Clear Cart
+								</button>
+							</div>
 						</div>
 					)}
 				</div>
