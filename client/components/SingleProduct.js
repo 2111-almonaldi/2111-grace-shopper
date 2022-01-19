32978f60dcfffd84b2909993b71cb1dc912e85cd
kayda <kejda.b.celaj@gmail.com>
2022-01-19 14:40:34 -0500

    feat: styling for singleProduct on <600

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -19,14 +19,14 @@ export class SingleProduct extends Component {
     const { singleProduct } = this.props;
 
     return (
-      <div className="single-product">
-        <div className="product" key={singleProduct.id}>
+      <div>
+        <div className="single-product" key={singleProduct.id}>
           <img
             src={singleProduct.imageUrl}
             style={{ width: "200px", height: "200px" }}
           />
-          <div className="product-info">
-            <div className="product-desc">
+          <div className="single-product-info">
+            <div className="single-product-desc">
               <h1>{singleProduct.name}</h1>
               <div>{singleProduct.description}</div>
             </div>
