b318597ea5bd763f87eb4616ba4b8fc8b371a1c2
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-01-18 21:41:02 -0500

    UserMain - connected state

--- client/components/User/UserMain.js
+++ client/components/User/UserMain.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default function UserMain() {
-	<div>hello user</div>;
+export class UserMain extends Component {
+	render() {
+		const { auth } = this.props;
+
+		return <div>hello, {auth.lastName}!</div>;
+	}
 }
+
+const mapState = (state) => ({
+	auth: state.auth,
+});
+
+export default connect(mapState)(UserMain);
