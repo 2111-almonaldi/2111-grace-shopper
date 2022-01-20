cc1024618f09ad36f40b8112a85980a4570e0c2f
Nicole DeSantis <nicolemdesantis13@gmail.com>
2022-01-20 07:32:34 +0000

    updated redirection at checkout

--- client/components/Checkout/CardForm.js
+++ client/components/Checkout/CardForm.js
@@ -1,86 +1,91 @@
-import React, { useMemo } from 'react';
-import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
-import '../../../public/cardForm.css';
-
-import useResponsiveFontSize from './useResponsiveFontSize';
+import React, { useMemo } from "react";
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
+import "../../../public/cardForm.css";
+import { useDispatch } from "react-redux";
+import { checkout } from "../../store/order";
+import useResponsiveFontSize from "./useResponsiveFontSize";
 
 const useOptions = () => {
-	const fontSize = useResponsiveFontSize();
-	const options = useMemo(
-		() => ({
-			style: {
-				base: {
-					fontSize,
-					color: '#424770',
-					letterSpacing: '0.025em',
-					fontFamily: 'Source Code Pro, monospace',
-					'::placeholder': {
-						color: '#aab7c4',
-					},
-				},
-				invalid: {
-					color: '#9e2146',
-				},
-			},
-		}),
-		[fontSize]
-	);
+  const fontSize = useResponsiveFontSize();
+  const options = useMemo(
+    () => ({
+      style: {
+        base: {
+          fontSize,
+          color: "#424770",
+          letterSpacing: "0.025em",
+          fontFamily: "Source Code Pro, monospace",
+          "::placeholder": {
+            color: "#aab7c4",
+          },
+        },
+        invalid: {
+          color: "#9e2146",
+        },
+      },
+    }),
+    [fontSize]
+  );
 
-	return options;
+  return options;
 };
 
 const CardForm = () => {
-	const stripe = useStripe();
-	const elements = useElements();
-	const options = useOptions();
+  const stripe = useStripe();
+  const elements = useElements();
+  const options = useOptions();
+  const dispatch = useDispatch();
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-		if (!stripe || !elements) {
-			// Stripe.js has not loaded yet. Make sure to disable
-			// form submission until Stripe.js has loaded.
-			return;
-		}
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+    try {
+      const payload = await stripe.createPaymentMethod({
+        type: "card",
+        card: elements.getElement(CardElement),
+      });
+      alert("Success! Get your popcorn ready!");
+      dispatch(checkout);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-		const payload = await stripe.createPaymentMethod({
-			type: 'card',
-			card: elements.getElement(CardElement),
-		});
-
-		console.log('[PaymentMethod]', payload);
-	};
-
-	return (
-		<form
-			onSubmit={handleSubmit}
-			action="/create-checkout-session"
-			method="POST"
-		>
-			<label>
-				Card details
-				<CardElement
-					options={options}
-					onReady={() => {
-						console.log('CardElement [ready]');
-					}}
-					onChange={(event) => {
-						console.log('CardElement [change]', event);
-					}}
-					onBlur={() => {
-						console.log('CardElement [blur]');
-					}}
-					onFocus={() => {
-						console.log('CardElement [focus]');
-					}}
-				/>
-			</label>
-			<button type="submit" id="checkout-button" disabled={!stripe}>
-				Pay
-			</button>
-			;
-		</form>
-	);
+  return (
+    <form
+      onSubmit={handleSubmit}
+      action="/create-checkout-session"
+      method="POST"
+    >
+      <label>
+        Card details
+        <CardElement
+          options={options}
+          onReady={() => {
+            console.log("CardElement [ready]");
+          }}
+          onChange={(event) => {
+            console.log("CardElement [change]", event);
+          }}
+          onBlur={() => {
+            console.log("CardElement [blur]");
+          }}
+          onFocus={() => {
+            console.log("CardElement [focus]");
+          }}
+        />
+      </label>
+      <button type="submit" id="checkout-button" disabled={!stripe}>
+        Pay
+      </button>
+      ;
+    </form>
+  );
 };
 
 export default CardForm;
--- client/components/Checkout/GuestCheckout.js
+++ client/components/Checkout/GuestCheckout.js
@@ -1,54 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useSelector, useDispatch } from "react-redux";
+import { checkoutOrder } from "../../store/order";
 
 export default function GuestCheckout() {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { order } = useSelector((state) => state.orders);
+  const dispatch = useDispatch();
 
-	return (
-		<form
-			onSubmit={handleSubmit((data) => {
-				console.log(data);
-			})}
-		>
-			<input
-				{...register('customerEmail', {
-					required: 'This field is required',
-				})}
-				placeholder="Email"
-			/>
-			<div>{errors.customerEmail?.message}</div>
-			<input
-				{...register('customerName', { required: 'This field is required' })}
-				placeholder="Name"
-			/>
-			<div>{errors.customerName?.message}</div>
-			<input
-				{...register('customerAddress', {
-					required: 'This field is required',
-				})}
-				placeholder="Address Line 1"
-			/>
-			<div>{errors.customerAddress?.message}</div>
-			<input
-				{...register('customerCity', { required: 'This field is required' })}
-				placeholder="City"
-			/>
-			<div>{errors.customerCity?.message}</div>
-			<input
-				{...register('customerState', { required: 'This field is required' })}
-				placeholder="State"
-			/>
-			<div>{errors.customerState?.message}</div>
-			<input {...register('customerZip')} placeholder="Zip Code" />
-			<input
-				{...register('customerPhone', { required: false })}
-				placeholder="Phone (Optional)"
-			/>
-			<input type="submit" />
-		</form>
-	);
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        dispatch(checkoutOrder(data));
+      })}
+    >
+      <input
+        {...register("customerEmail", {
+          required: "This field is required",
+        })}
+        placeholder="Email"
+      />
+      <div>{errors.customerEmail?.message}</div>
+      <input
+        {...register("customerName", { required: "This field is required" })}
+        placeholder="Name"
+      />
+      <div>{errors.customerName?.message}</div>
+      <input
+        {...register("customerAddress", {
+          required: "This field is required",
+        })}
+        placeholder="Address Line 1"
+      />
+      <div>{errors.customerAddress?.message}</div>
+      <input
+        {...register("customerCity", { required: "This field is required" })}
+        placeholder="City"
+      />
+      <div>{errors.customerCity?.message}</div>
+      <input
+        {...register("customerState", { required: "This field is required" })}
+        placeholder="State"
+      />
+      <div>{errors.customerState?.message}</div>
+      <input {...register("customerZip")} placeholder="Zip Code" />
+      <input
+        {...register("customerPhone", { required: false })}
+        placeholder="Phone (Optional)"
+      />
+      <input type="submit" />
+    </form>
+  );
 }
